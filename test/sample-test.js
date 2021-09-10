e200ad04e1ee22a1719e235e5739460759365d60
Chris Schnabel <chrisjschnabel@gmail.com>
2021-09-09 20:40:44 -0400

    Renamed contract variable in test file.

--- test/sample-test.js
+++ test/sample-test.js
@@ -11,7 +11,7 @@ function getValue() {
 
 describe("CLIPArt paint", function () {
   let CLIPArt;
-  let hardhatContract;
+  let contract;
   let owner;
   let addr1;
   let addr2;
@@ -24,43 +24,43 @@ describe("CLIPArt paint", function () {
     const maxTotal = 5;
     const maxPerAddress = 2;
 
-    hardhatContract = await CLIPArt.deploy(maxTotal, maxPerAddress);
+    contract = await CLIPArt.deploy(maxTotal, maxPerAddress);
   });
 
   describe("Deployment", function() {
     it("Deployment should set max number of pieces.", async function() {
-      expect(await hardhatContract._maxNumberOfPieces()).to.equal(5);
-      expect(await hardhatContract._maxNumberOfPiecesPerAddress()).to.equal(2);
+      expect(await contract._maxNumberOfPieces()).to.equal(5);
+      expect(await contract._maxNumberOfPiecesPerAddress()).to.equal(2);
     });
   });
 
 
   describe("Minting", function() {
     it("Balance should increment after painting.", async function() {
-      await hardhatContract.paint(getValue());
-      let balance = await hardhatContract.balanceOf(owner.address);
+      await contract.paint(getValue());
+      let balance = await contract.balanceOf(owner.address);
       expect(balance).to.equal(1);
-      let paintingOwner = await hardhatContract.ownerOf(1);
+      let paintingOwner = await contract.ownerOf(1);
       expect(paintingOwner).to.equal(owner.address);
 
       // Mint 2nd painting
-      await hardhatContract.paint(getValue());
-      balance = await hardhatContract.balanceOf(owner.address);
+      await contract.paint(getValue());
+      balance = await contract.balanceOf(owner.address);
       expect(balance).to.equal(2);
-      paintingOwner = await hardhatContract.ownerOf(2);
+      paintingOwner = await contract.ownerOf(2);
       expect(paintingOwner).to.equal(owner.address);
     });
 
     it("Should set tokenURI correctly.", async function() {
-      await hardhatContract.paint(getValue());
+      await contract.paint(getValue());
       let id = 1;
-      let tokenURI = await hardhatContract.tokenURI(1);
+      let tokenURI = await contract.tokenURI(1);
       expect(tokenURI).to.equal("http://localhost:8080/test/1");
     });
 
     it("Should not allow a mint without sending any ether.", async function() {
       await expect(
-        hardhatContract.paint()
+        contract.paint()
       ).to.be.revertedWith("You did not send enough ether to interest the painter. Please send at least 0.1 ether.");
     });
 
@@ -69,33 +69,33 @@ describe("CLIPArt paint", function () {
         value: ethers.utils.parseEther("0.01")
       };
       await expect(
-        hardhatContract.paint(overrides)
+        contract.paint(overrides)
       ).to.be.revertedWith("You did not send enough ether to interest the painter. Please send at least 0.1 ether.");
     });
 
     it("Should allow a mint when sending 0.1 ether.", async function() {
-      await hardhatContract.paint(getValue());
-      let balance = await hardhatContract.balanceOf(owner.address);
+      await contract.paint(getValue());
+      let balance = await contract.balanceOf(owner.address);
       expect(balance).to.equal(1);
     });
 
     it("Should not allow more than the max number of total mints.", async function() {
-      await hardhatContract.paint(getValue());
-      await hardhatContract.connect(addr1).paint(getValue());
-      await hardhatContract.connect(addr2).paint(getValue());
-      await hardhatContract.connect(addrs[0]).paint(getValue());
-      await hardhatContract.connect(addrs[1]).paint(getValue());
+      await contract.paint(getValue());
+      await contract.connect(addr1).paint(getValue());
+      await contract.connect(addr2).paint(getValue());
+      await contract.connect(addrs[0]).paint(getValue());
+      await contract.connect(addrs[1]).paint(getValue());
       await expect(
-        hardhatContract.connect(addrs[2]).paint(getValue())
+        contract.connect(addrs[2]).paint(getValue())
       ).to.be.revertedWith("All pieces have already been minted.");
     });
 
     it("Should not allow more than the max number of mints per address.", async function() {
-      await hardhatContract.paint(getValue());
-      await hardhatContract.paint(getValue());
+      await contract.paint(getValue());
+      await contract.paint(getValue());
 
       await expect(
-        hardhatContract.paint(getValue())
+        contract.paint(getValue())
       ).to.be.revertedWith("You have already minted the max number of pieces per address.");
     });
   });
